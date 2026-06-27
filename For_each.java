21a689ff4fa5b1b415fed29b1ae1caf1e48f05d9
Himanshusing-h <himanshusingh1177q@gmail.com>
2026-06-27 11:00:24 +0530

    clmn

--- For_each.java
+++ For_each.java
@@ -11,11 +11,12 @@ public class For_each {
                 arr[i][j] = sc.nextInt();
             }
         }
+        //{{2,1,3},{7,8,9,},{7,5,1}}
         //array of arrays
-        //this will take one array
+        //2d array mai se 1d ko uthaya
         for(int[] a:arr)
         {
-            //this is for another array
+        //printing krdi
             for (int ele:a){
                 System.out.print(ele+" ");
             }
--- snake_printting_column.java
+++ snake_printting_column.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-public class Snake_printing {
+
+public class snake_printting_column{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[3][3];
+        int[][] arr = new int[3][4];
         for (int i = 0; i < 3; i++) {
             System.out.println("Enter "+(i+1)+"th rows");
-            for (int j = 0; j < 3; j++) {
+            for (int j = 0; j < 4; j++) {
 
                 System.out.println("Enter "+(j+1)+"th element");
                 arr[i][j] = sc.nextInt();
@@ -14,20 +15,20 @@ public class Snake_printing {
         }
         //print row with even idex as it is
         //print in reverse which has odd index
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 4; i++) {
             //checking odd or even index
             if(i%2==0){
                 //if even printing normally
-            for (int j = 0; j < 3; j++) {
-                System.out.print(arr[i][j]+" ");
-            }
+                for (int j = 0; j < 3; j++) {
+                    System.out.print(arr[j][i]+" ");
+                }
 
             }
             else{
                 //if odd printing reverse
-                for (int j=arr[0].length-1;j>=0;j--)
+                for (int j=arr.length-1;j>=0;j--)
                 {
-                    System.out.print(arr[i][j]+" ");
+                    System.out.print(arr[j][i]+" ");
                 }
             }
             System.out.println();
@@ -35,3 +36,5 @@ public class Snake_printing {
 
     }
 }
+
+
